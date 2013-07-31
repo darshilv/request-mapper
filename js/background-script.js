e72683defe02a5f83c9dfebdd95ac3d9d14c8310
Adam Putinski <adam.putinski@gmail.com>
2013-07-30 21:08:30 -0700

    Cleanup

--- js/background-script.js
+++ js/background-script.js
@@ -1,33 +1,26 @@
 (function () {
 
-    var mappers, storedMappers;
+    var mapper, mappers, storedMappers;
 
     storedMappers = window.localStorage.getItem('requestMapper');
 
-    if ( storedMappers !== null && storedMappers !== 'null' ) {
+    if (storedMappers !== null && storedMappers !== 'null') {
 
         mappers = JSON.parse(storedMappers);
 
-        for ( mapper in mappers ) {
-
+        for (mapper in mappers) {
             mappers[mapper].findRegEx = new RegExp(mappers[mapper].find);
-
         }
 
         chrome.webRequest.onBeforeRequest.addListener(function (details) {
-
-            for ( mapper in mappers ) {
-
-                if ( details.url.match(mappers[mapper].findRegEx) ) {
-
+            var mapper;
+            for (mapper in mappers) {
+                if (details.url.match(mappers[mapper].findRegEx)) {
                     return {
                         redirectUrl: details.url.replace(mappers[mapper].findRegEx, mappers[mapper].replace)
                     };
-
                 }
-
             }
-
         }, { urls: ['<all_urls>'] }, ['blocking']);
 
     }
